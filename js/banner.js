1ac65268aeaec480a76b12162c23505a1a5aa8da
Nathan <natms24@gmail.com>
2024-02-04 18:21:54 +0300

    Added amharic page and link effects

--- js/banner.js
+++ js/banner.js
@@ -2,21 +2,32 @@ const bannerContainer = document.getElementById('bannerContainer');
 const leftBtn = document.getElementById('leftBtn');
 const rightBtn = document.getElementById('rightBtn');
 
-const imgArr = ['1.png', '2.jpg', '3.jpg', '4.jpg']
+const imgArr = [
+    document.getElementById('imageOne'),
+    document.getElementById('imageTwo'),
+    document.getElementById('imageThree'),
+    document.getElementById('imageFour')
+]
 let currIdx = 0
 
 rightBtn.addEventListener('click', () => {
+    imgArr[currIdx].classList.remove('block')
+    imgArr[currIdx].classList.add('hidden')
     currIdx += 1
     if (currIdx === imgArr.length) {
         currIdx = 0
     }
-    bannerContainer.innerHTML = `<img src="images/${imgArr[currIdx]}" alt="graduates" class="w-full block">`
+    imgArr[currIdx].classList.remove('hidden')
+    imgArr[currIdx].classList.add('block')
 })
 
 leftBtn.addEventListener('click', () => {
+    imgArr[currIdx].classList.remove('block')
+    imgArr[currIdx].classList.add('hidden')
     currIdx -= 1
     if (currIdx === -1) {
         currIdx = imgArr.length - 1
     }
-    bannerContainer.innerHTML = `<img src="images/${imgArr[currIdx]}" alt="graduates" class="w-full block">`
+    imgArr[currIdx].classList.remove('hidden')
+    imgArr[currIdx].classList.add('block')
 })
